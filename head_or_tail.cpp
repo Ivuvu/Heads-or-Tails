7a8dd73bde7408ee893dffb53a756d5bde0588a8
Koichiro Ibuka <koichiro@wlan-napt-001.u-aizu.ac.jp>
2020-08-06 21:08:35 +0900

    HoT: Initial revision

---
+++ head_or_tail.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string.h>
+#include <time.h>
+using namespace std;
+
+inline void InitRand(){
+    srand((unsigned int)time(NULL));
+}
+
+int main(){
+    int head_cnt,tail_cnt;
+    head_cnt = tail_cnt = 0;
+
+    cout<<"Tossing a coin..."<<endl;
+    InitRand();
+
+    for(int i=0; i<3; i++){
+        int flag = rand()%2;
+        string res;
+        if(flag){
+            res = "Heads";
+            head_cnt++;
+        }else{
+            res = "Tails";
+            tail_cnt++;
+        }
+
+        cout<<"Round "<<i<<": "<<res<<endl;
+    }
+
+    cout<<"Heads : "<<head_cnt<<", ";
+    cout<<"Tails: "<<tail_cnt<<endl;
+}
